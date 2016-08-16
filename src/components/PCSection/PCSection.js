e771535d262fa6aa56f63028c9b793d13ca2a424
simonghales <simonghales@gmail.com>
2016-08-17 00:18:20 +1000

    basic child creation

--- src/components/PCSection/PCSection.js
+++ src/components/PCSection/PCSection.js
@@ -1,11 +1,49 @@
 import React from 'react'
 import classes from './PCSection.scss'
+import { returnComponentObject } from '../../utilities/component'
 
-export const PCSection = (props) => (
-  <div className={classes['PCSection']}>
-    <h1>PCSection</h1>
-    <h2>{props.data.label}</h2>
-  </div>
-)
+class PCSection extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.renderChildren = this.renderChildren.bind(this);
+
+  }
+
+  renderChildren() {
+
+    const { data } = this.props;
+
+    let children = [];
+
+    for(let i = 0, len = data.children.length; i < len; i++) {
+      let child = data.children[i];
+      children.push(
+        returnComponentObject(child)
+      );
+    }
+
+    return children;
+
+  }
+
+  render() {
+
+    const { data } = this.props;
+
+    return(
+      <div className={classes['PCSection']}>
+        <h1>PCSection</h1>
+        <h2>{data.label}</h2>
+        <div>
+          {this.renderChildren()}
+        </div>
+      </div>
+    );
+
+  }
+
+}
 
 export default PCSection
--- src/constants/component/types.js
+++ src/constants/component/types.js
@@ -3,3 +3,18 @@ export const PCHEADING        = 'PCHEADING'
 export const PCIMAGE          = 'PCIMAGE'
 export const PCPARAGRAPH      = 'PCPARAGRAPH'
 export const PCSECTION        = 'PCSECTION'
+
+export const COMPONENT_TYPES_SETTINGS = {
+  [PCHEADING]: {
+    childrenAllowed: false,
+  },
+  [PCIMAGE]: {
+    childrenAllowed: false,
+  },
+  [PCPARAGRAPH]: {
+    childrenAllowed: false,
+  },
+  [PCSECTION]: {
+    childrenAllowed: true,
+  },
+};
